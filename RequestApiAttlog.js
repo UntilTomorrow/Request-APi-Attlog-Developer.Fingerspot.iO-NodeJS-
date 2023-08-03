410c6b378dbf4f9b70cbc141dcb17d1bbde9c159
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-08-03 20:01:33 +0700

    Update RequestApiAttlog.js

--- RequestApiAttlog.js
+++ RequestApiAttlog.js
@@ -3,22 +3,23 @@ const express = require('express');
 const app = express();
 ////////////////////////
 
-////
 var request = require('request');
 var options = {
   'method': 'POST',
   'url': 'http://developer.fingerspot.io/api/get_attlog',
   'headers': {
-    'Authorization': 'Bearer N2KWNZN4XXIP8GV0',
+    'Authorization': 'Bearer xxxx',
     'Content-Type': 'application/json'
   },
-  body: JSON.stringify({"trans_id":"1", "cloud_id":"C269248053182536", "start_date":"2022-08-09", "end_date":"2022-08-09"})
+  body: JSON.stringify({"trans_id":"1", "cloud_id":"xxx", "start_date":"2023-07-15", "end_date":"2023-07-15"})
 
 };
 request(options, function (error, response) { 
   if (error) throw new Error(error);
  
-  console.log(JSON.stringify({"status": 200, "response": response.body}));
+  const parsedResponse = JSON.parse(response.body);
+  const formattedResponse = JSON.stringify({"status": 200, "response": parsedResponse}, null, 2);
+  console.log(formattedResponse);
 });
 
 app.listen(port, () => console.log('running http://localhost:' + port));
